7560c5d701a93e043770ebca881c2974ac2f6604
Cow Cat <91896578+InternetBot@users.noreply.github.com>
2026-08-20 05:15:14 -0400

    now yells the OG 67 sound  when you type in 67

--- main.c
+++ main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <Windows.h>
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h" //https://github.com/mackron/miniaudio real hero right here !
+#include "67.h"
 
 //6	0x36	6 key
 //7	0x37	7 key
+
+
+void play_67(void){
+	ma_engine engine;
+	ma_result result;
+
+	result = ma_engine_init(NULL, &engine);
+	if(result != MA_SUCCESS){
+		printf("we f'ed up \n");
+		return;
+	}
+
+	//decoder
+	ma_decoder decoder;
+	if(ma_decoder_init_memory(__67_SQlv2Xv_mp3, __67_SQlv2Xv_mp3_len, NULL, &decoder) != MA_SUCCESS){
+		printf("failed to decode \n");
+		ma_engine_uninit(&engine);
+		return;
+	}
+	
+	ma_sound sound;
+	ma_sound_init_from_data_source(&engine, &decoder, 0, NULL, &sound);
+	ma_sound_start(&sound);
+	
+	while(ma_sound_is_playing(&sound)){
+		ma_sleep(100);
+	}
+
+	ma_sound_uninit(&sound);
+	ma_decoder_uninit(&decoder);
+	ma_engine_uninit(&engine);
+	
+}
 
 int main()
 {
@@ -23,6 +59,7 @@ static int lastKey = 0;
 
 				if (lastKey == '6' && vKey == '7'){
 					printf("found 67\n");
+					play_67();
 				}
 
 				lastKey = vKey;
@@ -30,7 +67,6 @@ static int lastKey = 0;
 
             Sleep(50);
         }   
-
     } while (TRUE);   
 
     return 0;
